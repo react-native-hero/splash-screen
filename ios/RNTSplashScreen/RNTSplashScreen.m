8f2e336f3ed9eee587c1ffa8e7c351e67ce3065e
musicode <musicode@qq.com>
2026-03-29 02:05:27 +0800

    bump 0.1.2

--- ios/RNTSplashScreen/RNTSplashScreen.m
+++ ios/RNTSplashScreen/RNTSplashScreen.m
@@ -1,104 +1,123 @@
 #import "RNTSplashScreen.h"
 #import <React/RCTBridge.h>
 #import <React/RCTRootView.h>
+#import <React/RCTUtils.h>
+
+#if RCT_NEW_ARCH_ENABLED
+#import <React/RCTSurfaceHostingProxyRootView.h>
+#import <React/RCTSurfaceHostingView.h>
+#endif
 
 @implementation RNTSplashScreen
 
-static BOOL hasJavaScriptDidFailToLoadObserver = NO;
-static BOOL hasContentDidAppearObserver = YES;
-static BOOL isShowing = NO;
+#if RCT_NEW_ARCH_ENABLED
+static RCTSurfaceHostingProxyRootView *_rootView = nil;
+#else
+static UIView *_rootView = nil;
+#endif
 
-static RCTRootView *rootView = nil;
+static UIView *_loadingView = nil;
+static BOOL _fade = NO;
 
 + (BOOL)requiresMainQueueSetup {
-    return YES;
+    return NO;
 }
 
 - (dispatch_queue_t)methodQueue {
-    return dispatch_queue_create("com.github.reactnativehero.splash_screen", DISPATCH_QUEUE_SERIAL);
+    return dispatch_get_main_queue();
 }
 
-+ (void)show:(UIView *)view storyboardName:(NSString *)storyboardName {
-    
-    if (!hasJavaScriptDidFailToLoadObserver) {
-        
-        [NSNotificationCenter.defaultCenter addObserver:self
-                                               selector:@selector(onJavaScriptDidFailToLoad:)
-                                                   name:RCTJavaScriptDidFailToLoadNotification
-                                                 object:nil
-        ];
+// ==========================================
+// 核心逻辑方法 (供原生调用)
+// ==========================================
 
-        hasJavaScriptDidFailToLoadObserver = YES;
-        
++ (void)show:(UIView *)view storyboardName:(NSString *)storyboardName {
+    if (!_rootView && view) {
+#ifdef RCT_NEW_ARCH_ENABLED
+        _rootView = (RCTSurfaceHostingProxyRootView *)view;
+#else
+        _rootView = (RCTRootView *)view;
+#endif
+
+        // 设置加载视图
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:storyboardName bundle:nil];
+        _loadingView = [[storyboard instantiateInitialViewController] view];
+
+        if (_loadingView) {
+            _loadingView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+            _loadingView.frame = _rootView.bounds;
+            _loadingView.center = (CGPoint){CGRectGetMidX(_rootView.bounds), CGRectGetMidY(_rootView.bounds)};
+            _loadingView.hidden = NO;
+
+#if RCT_NEW_ARCH_ENABLED
+            [_rootView disableActivityIndicatorAutoHide:YES];
+            [_rootView setLoadingView:_loadingView];
+#else
+            [_rootView addSubview:_loadingView];
+#endif
+        }
     }
-    
-    if (hasContentDidAppearObserver) {
-        
-        [[NSNotificationCenter defaultCenter] removeObserver:view
-                                                      name:RCTContentDidAppearNotification
-                                                    object:view];
-        
-        hasContentDidAppearObserver = NO;
-        
-    }
-    
-    if (isShowing) {
-        return;
-    }
-    
-    rootView = (RCTRootView *)view;
-    
-    [self showLoadingView:storyboardName];
-    isShowing = YES;
-    
 }
 
 + (void)hide {
-    
-    if (!isShowing) {
+    // 如果没有加载视图或已经隐藏，直接返回
+    if (!_loadingView || _loadingView.hidden) {
         return;
     }
-    
-    [self hideLoadingView];
-    isShowing = NO;
-    
-    if (hasJavaScriptDidFailToLoadObserver) {
-        
-        [NSNotificationCenter.defaultCenter removeObserver:self
-                                                   name:RCTJavaScriptDidFailToLoadNotification
-                                                 object:nil
-        ];
 
-        hasJavaScriptDidFailToLoadObserver = NO;
-        
+    if (_fade) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [UIView transitionWithView:_rootView
+                              duration:0.250
+                               options:UIViewAnimationOptionTransitionCrossDissolve
+                            animations:^{
+                _loadingView.hidden = YES;
+            }
+                            completion:^(__unused BOOL finished) {
+                                [_loadingView removeFromSuperview];
+                                _loadingView = nil;
+                            }];
+        });
+    } else {
+        _loadingView.hidden = YES;
+        [_loadingView removeFromSuperview];
+        _loadingView = nil;
     }
-    
 }
 
-+ (void)showLoadingView:(NSString *)storyboardName {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:storyboardName bundle:nil];
-        UIView *loadingView = [[storyboard instantiateInitialViewController] view];
-        [rootView setLoadingView:loadingView];
-    });
-}
-
-+ (void)hideLoadingView {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        rootView.loadingView.hidden = YES;
-        [rootView.loadingView removeFromSuperview];
-        rootView.loadingView = nil;
-    });
-}
-
-+ (void)onJavaScriptDidFailToLoad:(NSNotification *)notification {
-    [RNTSplashScreen hide];
-}
+#pragma mark - Module Exports
 
 RCT_EXPORT_MODULE(RNTSplashScreen);
 
-RCT_EXPORT_METHOD(hide) {
-    [RNTSplashScreen hide];
+#if RCT_NEW_ARCH_ENABLED
+
+// 新架构导出
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params {
+    return std::make_shared<facebook::react::NativeRNTSplashScreenSpecJSI>(params);
 }
+
+// 新架构下的 hide 方法：无参数，无 Promise
+- (void)hide {
+    [self.class hide];
+}
+
+- (nonnull NSNumber *)isVisible {
+    return @(_loadingView != nil && !_loadingView.hidden);
+}
+
+#else
+
+// 旧架构导出
+// 旧架构下的 hide 方法：无参数，无 Promise
+RCT_EXPORT_METHOD(hide) {
+    [self.class hide];
+}
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(isVisible) {
+    return @(_loadingView != nil && !_loadingView.hidden);
+}
+
+#endif
 
 @end
